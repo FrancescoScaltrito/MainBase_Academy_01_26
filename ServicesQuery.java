3d504c1a68840215ca8bb55903d1cf2a6f20e100
Francesco Scaltrito <101185088+FrancescoScaltrito@users.noreply.github.com>
2026-02-06 16:55:56 +0100

    Add files via upload

--- ServicesQuery.java
+++ ServicesQuery.java
@@ -3,12 +3,15 @@ package com.betacom.services;
 import java.util.List;
 import java.util.Map;
 
+import com.betacom.dao.DipendentiDAO;
 import com.betacom.exceptions.AcademyException;
+import com.betacom.objects.dipendenti;
 import com.betacom.singleton.SQLConfiguration;
 import com.betacom.utils.SQLManager;
 
 public class ServicesQuery {
 	private SQLManager db = new SQLManager();
+	private DipendentiDAO dao  = new DipendentiDAO();
 	
 	public void executeQuery() throws Exception{
 		listTable();
@@ -20,15 +23,21 @@ public class ServicesQuery {
 		lT.forEach(t -> System.out.println(t));
 	}
 	
-	private void getDipendenti() throws AcademyException{
+	private void getDipendenti() throws AcademyException {
 		String query = SQLConfiguration.getInstance().getQuery("query.dipendenti");
 		System.out.println(query);
-		
+		/*
 		List<Map<String,Object>> res = db.list(query);
 		System.out.println("Numero di righe: "+res.size());
 		
 		for (Map<String, Object> it : res) {
 			System.out.println(it.get("nome")+ " "+ it.get("cognome"));
+		}*/
+		try {
+			List<dipendenti> lD = dao.findAll();
+			lD.forEach(d -> System.out.println(d));
+		} catch (Exception e) {
+			System.out.println("Errore trovato "+e.getMessage());
 		}
 	}
 }
